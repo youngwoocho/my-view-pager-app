d95e39746a9c4a522b6f04ff1a593c75b82ac1d8
조영우 <youngchorg@gmail.com>
2022-02-26 23:09:13 +0900

    Set up entryActivity to navigate to next screen

--- app/src/main/java/com/example/myviewpagerapp/EntryActivity.kt
+++ app/src/main/java/com/example/myviewpagerapp/EntryActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myviewpagerapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myviewpagerapp.databinding.ActivityEntryBinding
 
 class EntryActivity : AppCompatActivity() {
+    private val viewBinding by viewBindingDelegate(ActivityEntryBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entry)
+        setContentView(viewBinding.root)
+
+        viewBinding.buttonNextScreen.setOnClickListener {
+            startActivity(Intent(this, ViewPagerActivity::class.java))
+        }
     }
 }
