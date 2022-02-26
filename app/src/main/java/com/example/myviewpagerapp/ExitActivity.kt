3d511e2370691c2df86474f8c857b978a06627d3
조영우 <youngchorg@gmail.com>
2022-02-26 19:25:50 +0900

    Add entry, exit, and viewpager activity

---
+++ app/src/main/java/com/example/myviewpagerapp/ExitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myviewpagerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exit)
+    }
+}
