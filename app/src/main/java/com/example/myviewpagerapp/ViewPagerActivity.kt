f172779fc928dd5a742c36d69a37e079a2f2cc7b
조영우 <youngchorg@gmail.com>
2022-02-26 23:26:10 +0900

    Add basic layout and flow for the app

--- app/src/main/java/com/example/myviewpagerapp/ViewPagerActivity.kt
+++ app/src/main/java/com/example/myviewpagerapp/ViewPagerActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myviewpagerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myviewpagerapp.databinding.ActivityViewPagerBinding
 
 class ViewPagerActivity : AppCompatActivity() {
+    private val viewBinding by viewBindingDelegate(ActivityViewPagerBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_pager)
+        setContentView(viewBinding.root)
+
+        viewBinding.buttonViewpagerNextScreen.setOnClickListener {
+            startActivity(Intent(this, ExitActivity::class.java))
+        }
     }
 }
