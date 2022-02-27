d8e7b1646fa45c1e6246c1f1cff6d47a81849c2d
조영우 <youngchorg@gmail.com>
2022-02-27 21:11:38 +0900

    Add toolbar for ExitActivity

--- app/src/main/java/com/example/myviewpagerapp/ExitActivity.kt
+++ app/src/main/java/com/example/myviewpagerapp/ExitActivity.kt
@@ -2,10 +2,21 @@ package com.example.myviewpagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.example.myviewpagerapp.databinding.ActivityExitBinding
 
 class ExitActivity : AppCompatActivity() {
+    private val viewBinding by viewBindingDelegate(ActivityExitBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exit)
+        setContentView(viewBinding.root)
+
+        setSupportActionBar(viewBinding.toolBarExit)
+        viewBinding.toolBarExit.navigationIcon =
+            ContextCompat.getDrawable(this, R.drawable.back_arrow)
+        viewBinding.toolBarExit.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
